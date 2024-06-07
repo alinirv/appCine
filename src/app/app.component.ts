08dcb559d22b02837186a5799aa2e085aa377883
arv <alini.vicente@yahoo.com.br>
2024-06-06 21:56:21 -0300

    refector: add heder in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
+import { MovieComponent } from './components/movie/movie.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent ]
+    imports: [RouterOutlet, MovieComponent, MatToolbarModule]
 })
 export class AppComponent {
   title = 'cineStar';
