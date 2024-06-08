0b6180e6cecc8cbfd377669af19e539bc96ea7bd
MuriloSdaSilva <murilor72003@outlook.com>
2024-06-08 20:06:42 -0300

    feat: implementa metodos de Ingresso

--- src/app/ingresso/ingresso.component.ts
+++ src/app/ingresso/ingresso.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { Ingresso } from '../model/Ingresso.model';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-ingresso',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './ingresso.component.html',
   styleUrl: './ingresso.component.css'
 })
 export class IngressoComponent {
   ingresso: Ingresso;
-
   constructor(){
     this.ingresso = new Ingresso;
+    this.ingresso.preco = 28
+    this.ingresso.exibicao = new Date('2024-07-09')
   }
 
+  getExibicao(){
+    return this.ingresso.exibicao;
+  }
+
+  getPreco(){
+    return this.ingresso.preco;
+  }
 }
