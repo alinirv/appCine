7be7eecc738c6d89365a8a4d868aa1e08245c532
MuriloSdaSilva <murilor72003@outlook.com>
2024-06-08 20:07:17 -0300

    feat: implementa ingresso no AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { MovieComponent } from './components/movie/movie.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { IngressoComponent } from './ingresso/ingresso.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MovieComponent, MatToolbarModule]
+    imports: [RouterOutlet, MovieComponent, MatToolbarModule,IngressoComponent]
 })
 export class AppComponent {
   title = 'cineStar';
