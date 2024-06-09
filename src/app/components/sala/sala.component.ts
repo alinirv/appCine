72b969d3d978414fd8e3162be66707242ac90e16
fernando <fernando.candido@aluno.ifsp.edu.br>
2024-06-09 02:24:44 -0300

    refector:  ajuste sala

--- src/app/components/sala/sala.component.ts
+++ src/app/components/sala/sala.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SharedMoviesService } from '../../service/shared-movies.service';
+import { Filme } from '../../model/Filme.model';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-sala',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule,MatCardModule,MatToolbarModule],
   templateUrl: './sala.component.html',
   styleUrl: './sala.component.css'
 })
 
 export class SalaComponent implements OnInit{
+  listaFilme: Filme[] = [];
 
-  
+  constructor(private movieSharedService: SharedMoviesService) {}
+
   ngOnInit(): void {
-    //throw new Error('Method not implemented.');
+    this.movieSharedService.filmes$.subscribe((filmes: Filme[]) => {
+      this.listaFilme = filmes;
+    });
+    this.movieSharedService.loadFilmes();
+    console.log(this.listaFilme)
   }
 
+
+  getFilme(id: number): Filme | undefined {
+    return this.listaFilme.find(filme => filme.id === id);
+  }
+  
+
 }
+
+
