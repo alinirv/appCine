e500306e0292e687d5e2a3881261f1a1edb5ba13
arv <alini.vicente@yahoo.com.br>
2024-06-09 00:44:04 -0300

    refector: ajusta componente filmes

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit, inject } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { Filme } from '../../model/Filme.model';
 import { MovieService } from '../../service/movie.service';
 import { SharedMoviesService } from '../../service/shared-movies.service';
@@ -6,11 +7,12 @@ import { SharedMoviesService } from '../../service/shared-movies.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie',
   standalone: true,
-  imports: [ MatCardModule, MatToolbarModule, MatGridListModule],
+  imports: [ MatCardModule, MatToolbarModule, MatGridListModule,MatIconModule, NgFor, NgIf],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
@@ -27,4 +29,16 @@ export class MovieComponent  implements OnInit {
     this.movieSharedService.loadFilmes();
   }
 
+  getStars(avaliacao: number): string[] {
+    const fullStars = Math.min(5, Math.max(1, Math.floor(avaliacao / 2)));
+    const emptyStars = 5 - fullStars;
+  
+    return [...Array(fullStars).fill('full'),...Array(emptyStars).fill('empty')];
+  }
+
+  trackById(index: number, star: string) {
+    return index;
+  }
+  
 }
+
