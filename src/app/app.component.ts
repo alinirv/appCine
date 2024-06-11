8ed0dd6dbd04509c68ceb0da906e7c959d94b019
arv <alini.vicente@yahoo.com.br>
2024-06-10 21:35:15 -0300

    fix: corrige recarregamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { MovieComponent } from './components/movie/movie.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { IngressoComponent } from './components/ingresso/ingresso.component';
 import { SalaComponent } from './components/sala/sala.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-import { Route } from '@angular/router';
-
+import { RouterOutlet } from '@angular/router';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, MovieComponent, MatToolbarModule,IngressoComponent,
-      SalaComponent,MatTabsModule, RouterModule, MatIconModule]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet, MovieComponent, MatToolbarModule, IngressoComponent, 
+    SalaComponent, MatTabsModule, RouterModule, MatIconModule]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'CineStar';
+  isPopState = false;
+  
+  constructor(private router: Router, private locStrat: LocationStrategy) { }
+  
+  ngOnInit(): void {
+	this.locStrat.onPopState(() => {
+	  this.isPopState = true;
+	});
+	
+	this.router.events.subscribe(event => {
+	  // Scroll to top if accessing a page, not via browser history stack
+	  if (event instanceof NavigationEnd && !this.isPopState) {
+		window.scrollTo(0, 0);
+		this.isPopState = false;
+	  }
+	  
+	  // Ensures that isPopState is reset
+	  if (event instanceof NavigationEnd) {
+		this.isPopState = false;
+	  }
+	});
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { SalaComponent } from './components/sala/sala.component';
 import { IngressoComponent } from './components/ingresso/ingresso.component'; 
 
 export const routes: Routes = [
-    { path: 'home', component: AppComponent },
     { path: 'movies', component: MovieComponent },
     { path: 'salas', component: SalaComponent },
     { path: 'ingressos', component: IngressoComponent }
